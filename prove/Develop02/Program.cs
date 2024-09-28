87d0b719b7d35bb8fe170692b1c7ebac9c4cf7a1
Bridger Fronk <bridgerfronk@gmail.com>
2024-09-28 04:41:54 -0400

    Finished prompt generator

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Entry entry = new Entry();
+        PromptGenerator prompts = new PromptGenerator();
+        prompts._prompts.Add("How was your day?");
+        prompts._prompts.Add("What was for breakfast?");
+        prompts._prompts.Add("Who is your crush?");
+        prompts._prompts.Add("Do you have a pet?");
+        prompts._prompts.Add("What is your favorite moment?");
+        entry._promptText = prompts.GetPrompt();
+        Console.WriteLine(entry._promptText);
     }
 }
